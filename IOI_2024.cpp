4af7b7bdc12283c51cfcd6104fce72296435846c
palakgupta111 <palakgupta955442@gmail.com>
2024-09-14 13:06:04 +0530

    codechef solved

---
+++ IOI_2024.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+
+    if(t>=1 && t<=8){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
